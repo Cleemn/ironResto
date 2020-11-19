d9c2424417189f81669c6957718a3416046af158
kardelen-karatas <kardelen.karatas@gmail.com>
2020-11-19 21:00:21 +0100

    post signup

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,28 +1,48 @@
-
-const express = require('express');
+const express = require("express");
 const authRoutes = express.Router();
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
+const User = require("../models/User.model");
 
-authRoutes.post('/signup', (req, res, next) => {
-    res.status(400).json({message:"post signup"})
-})
+authRoutes.post("/signup", (req, res, next) => {
+  const { username, email, password, phone, type } = req.body;
 
-authRoutes.post('/login', (req, res, next) => {
-    res.status(400).json({message:"post login"})
-})
+  if (!username || !email || !password || !phone || !type) {
+    res.status(400).json({ message: "Provide all the informations" });
+  }
 
-authRoutes.get('/loggedin', (req, res, next) => {
-    res.status(400).json({message:"get loggedin"})
-})
+  const salt = bcrypt.genSaltSync(10);
+  const passwordHash = bcrypt.hashSync(password, salt);
 
-authRoutes.post('/logout', (req, res, next) => {
-    res.status(400).json({message:"post logout"})
-})
+  User.create({ username, email, passwordHash, phone, type })
+    .then((userFromDB) => {
+      res.status(200).json(userFromDB);
+    })
+    .catch((error) => {
+      if (error.code === 11000) {
+        res.status(500).json({
+          errorMessage:
+            "Username and email need to be unique. Either username or email is already used.",
+        });
+      } else {
+        res.status(400).json({ message: "Create user went wrong" });
+      }
+    });
+});
 
-authRoutes.post('/user:id', (req, res, next) => {
-    res.status(400).json({message:"post user:id"})
-})
+authRoutes.post("/login", (req, res, next) => {
+  res.status(400).json({ message: "post login" });
+});
 
+authRoutes.get("/loggedin", (req, res, next) => {
+  res.status(400).json({ message: "get loggedin" });
+});
 
+authRoutes.post("/logout", (req, res, next) => {
+  res.status(400).json({ message: "post logout" });
+});
+
+authRoutes.post("/user:id", (req, res, next) => {
+  res.status(400).json({ message: "post user:id" });
+});
 
 module.exports = authRoutes;

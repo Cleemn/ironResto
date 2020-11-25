2db55e59487de16ee4e5b9fae96d70936f04f657
kardelen-karatas <kardelen.karatas@gmail.com>
2020-11-25 23:56:51 +0100

    order routes : GET / PUT

--- routes/order-routes.js
+++ routes/order-routes.js
@@ -1,21 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const orderRoutes = express.Router();
-const User = require("../models/User.model");
 const Order = require("../models/Order.model");
 const Product = require("../models/Product.model");
-const mongoose = require("mongoose");
-const { isValidObjectId } = require("mongoose");
 
 orderRoutes.post("/orders", (req, res, next) => {
-  const { items } = req.body;
-  // items = [{id : 1, quantity: 1}, {id : 2, quantity: 2} ]
+  const { items } = req.body; // items = [{id : 1, quantity: 1}, {id : 2, quantity: 2} ]
 
-    if (!req.session.user) {
-      res
-        .status(403)
-        .json({ message: "Vous n'etes pas connecté. Veuillez vous connecter." });
-      return;
-    }
+  if (!req.session.user) {
+    res
+      .status(403)
+      .json({ message: "Vous n'etes pas connecté. Veuillez vous connecter." });
+    return;
+  }
 
   let promises = [];
   for (item of items) {
@@ -25,7 +23,6 @@ orderRoutes.post("/orders", (req, res, next) => {
 
   Promise.all(promises)
     .then((products) => {
-
       for (product of products) {
         items.map((item) => {
           if (product._id == item.id) {
@@ -34,10 +31,12 @@ orderRoutes.post("/orders", (req, res, next) => {
         });
       }
 
-      let totalPrice = items.reduce((acc, item) => {return acc + item.price*item.quantity}, 0) 
+      let totalPrice = items.reduce((acc, item) => {
+        return acc + item.price * item.quantity;
+      }, 0);
 
       newOrder = {
-        user_id : req.session.user,
+        user_id: req.session.user,
         items: items,
         total_price: totalPrice,
         date: Date.now(),
@@ -49,26 +48,84 @@ orderRoutes.post("/orders", (req, res, next) => {
           res.status(200).json(order);
         })
         .catch((err) => res.status(500).json({ message: err.message }));
-
     })
     .catch((err) => res.status(500).json({ message: err.message }));
 });
 
 orderRoutes.get("/orders", (req, res, next) => {
-  return;
+  if (!req.session.user || req.session.user.type === "user") {
+    res.status(403).json({ message: "Not autorised." });
+    return;
+  }
+
+  Order.find() // faut on mettre une filtre de la journée ? {time:Date.now}
+    .then((allOrders) => {
+      res.status(200).json(allOrders);
+    })
+    .catch((err) => res.status(500).json({ message: err.message }));
 });
 
-orderRoutes.get("/orders:id", (req, res, next) => {
-  return;
+orderRoutes.get("/orders/:id", (req, res, next) => {
+  const orderId = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(orderId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  if (!req.session.user) {
+    res.status(403).json({ message: "Not autorised." });
+    return;
+  }
+
+  Order.findById(orderId)
+    .then((selectedOrder) => {
+      if (
+        req.session.user.type === "user" &&
+        !(selectedOrder.user_id == req.session.user._id)
+      ) {
+        res.status(403).json({ message: "Not autorised." });
+        return;
+      }
+
+      res.status(200).json(selectedOrder);
+    })
+    .catch((err) => res.status(500).json({ message: err.message }));
 });
 
-orderRoutes.put("/orders:id", (req, res, next) => {
-  // si user type est rastaurateur
-  // alors il peut mettre changé le status comme il veut
-  // si user type est client
-  // alors il peut changé le satut que à l'annulé
-  // sinon "not autorised" et demander de login user
-  return;
+orderRoutes.put("/orders/:id", (req, res, next) => {
+  const orderId = req.params.id;
+  const newStatus = req.body.status;
+
+  if (!mongoose.Types.ObjectId.isValid(orderId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  if (!req.session.user) {
+    res.status(403).json({ message: "Not autorised." });
+    return;
+  }
+
+  Order.findByIdAndUpdate(orderId, { status: newStatus }, { new: true })
+    .then((updatedOrder) => {
+      if (req.session.user.type === "user") {
+
+        if (!(updatedOrder.user_id == req.session.user._id)) {
+          res.status(403).json({ message: "Not autorised." });
+          return;
+        }
+
+        else if(newStatus !== "annule"){
+          res.status(401).json({ message: "Not autorised." });
+          return;
+        }
+
+      }
+
+      res.status(200).json(updatedOrder);
+    })
+    .catch((err) => res.status(500).json({ message: err.message }));
 });
 
 module.exports = orderRoutes;

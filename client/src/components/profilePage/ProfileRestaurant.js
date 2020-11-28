9485851f5782ce94ed88c2ac9aa0a5aca8ba0139
kardelen-karatas <kardelen.karatas@gmail.com>
2020-11-28 14:50:43 +0100

    init profile pages

---
+++ client/src/components/profilePage/ProfileRestaurant.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+class ProfileRestaurant extends React.Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="profile-restaurant">
+        <p>ProfileRestaurant</p>
+      </div>
+    );
+  }
+}
+
+export default ProfileRestaurant;

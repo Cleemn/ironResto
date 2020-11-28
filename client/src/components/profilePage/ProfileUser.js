d240c3c90216649ead8f0b455a88b67814510ced
kardelen-karatas <kardelen.karatas@gmail.com>
2020-11-28 14:50:43 +0100

    init profile pages

---
+++ client/src/components/profilePage/ProfileUser.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+class ProfileUser extends React.Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="profile-user">
+        <p>ProfileUser</p>
+      </div>
+    );
+  }
+}
+
+export default ProfileUser;

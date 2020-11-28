d0ba6ef8073be99e1085a62ba29644a873ba56a7
Clementine <clementinenocton@hotmail.fr>
2020-11-28 14:56:59 +0100

    Product detail page desktop ok

--- client/src/components/products/ProductDetails.js
+++ client/src/components/products/ProductDetails.js
@@ -43,25 +43,32 @@ class ProductDetails extends Component {
           <Link to={'/'}><img src="../white-arrow.png" alt=""/></Link>
           <img src="../menu.png" alt=""/>
         </div>
-        <img className="product-image" src={this.state.photo} alt=""/>
         <div className="product-card">
-          <div className="d-flex justify-content-between">
-            <h5>{this.state.name}</h5>
-            <div className="d-flex justify-content-evenly">
-              <h5 className="pr-2 remove" onClick={this.DecreaseItem}>-</h5>
-              <h5 className="pr-2 quantity">{ this.state.clicks }</h5>
-              <h5 className="pr-2 add" onClick={this.IncrementItem}>+</h5>
+          <img className="product-image" src={this.state.photo} alt=""/>
+          <div className="product-description">
+            <div className="left"></div>
+            <div className="right d-flex flex-column justify-content-between" style={{height: '100%'}}>
+              <div>
+                <div className="d-flex justify-content-between">
+                  <h5>{this.state.name}</h5>
+                  <div className="d-flex justify-content-evenly">
+                    <button style={{border: 'none'}} className="pr-2 remove" onClick={this.DecreaseItem}>-</button>
+                    <button style={{border: 'none'}} className="pr-2 quantity">{ this.state.clicks }</button>
+                    <button style={{border: 'none'}} className="pr-2 add" onClick={this.IncrementItem}>+</button>
+                  </div>
+                </div>
+                <h6>{this.state.price} €</h6>
+                <div className="portion d-flex justify-content-center">
+                  <p>{this.state.portion} pers. - {this.state.calories} kcal</p>
+                </div>
+                <div className="description">
+                  <h5>Description</h5>
+                  <p>{this.state.description}</p>
+                </div>
+              </div>
+              <button type="submit" className="btn btn-orange btn-block">Ajouter au panier</button>
             </div>
           </div>
-          <h6>{this.state.price} €</h6>
-          <div className="portion d-flex justify-content-center">
-            <p>{this.state.portion} pers. - {this.state.calories} kcal</p>
-          </div>
-          <div className="description">
-            <h5>Description</h5>
-            <p>{this.state.description}</p>
-          </div>
-          <button type="submit" className="btn btn-orange btn-block">Ajouter au panier</button>
         </div>
       </div>
       <style type="text/css">

5e16bde5a9e6dfc0cc0f92ab5f6cb5cb54f669fd
kardelen-karatas <kardelen.karatas@gmail.com>
2020-11-29 20:50:20 +0100

    accordion pour chaque commande

---
+++ client/src/components/orders/UserOrderList.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+class UserOrderList extends React.Component{
+    
+}
--- client/src/components/profilePage/ProfileUser.js
+++ client/src/components/profilePage/ProfileUser.js
@@ -2,10 +2,43 @@ import React from "react";
 import { orders } from "../services/order-service";
 // import { Link } from "react-router-dom";
 
+const frenchDays = [
+  "Dimanche",
+  "Lundi",
+  "Mardi",
+  "Mercredi",
+  "Jeudi",
+  "Vendredi",
+  "Samedi",
+];
+
+const frenchMonths = [
+  "Janvier",
+  "Février",
+  "Mars",
+  "Avril",
+  "Mai",
+  "Juin",
+  "Juillet",
+  "Août",
+  "Septembre",
+  "Octobre",
+  "Novembre",
+  "Décembre",
+];
 class ProfileUser extends React.Component {
   state = {
     orders: [],
   };
+
+  convertDate(date) {
+    let orderDate = new Date(Date(date));
+    let dayWeek = frenchDays[orderDate.getDay()];
+    let day = orderDate.getDate();
+    let month = frenchMonths[orderDate.getMonth() + 1];
+    let year = orderDate.getFullYear();
+    return { dayWeek, day, month, year };
+  }
 
   componentDidMount() {
     orders().then((userOrders) => {
@@ -14,15 +47,69 @@ class ProfileUser extends React.Component {
   }
 
   render() {
-    return (
-      <div className="profile-restaurant">
-      <p>User Profile</p>
+    console.log(this.state.orders);
 
-        {/* <lu>
-          {this.state.orders.map((order) => {
-            <li>{order.name}</li>;
-          })}
-        </lu> */}
+    return (
+      <div className="profile-user">
+        <div {...{ className: "wrapper" }}>
+          <ul {...{ className: "accordion-list" }}>
+            {this.state.orders.map((order, key) => {
+              const { dayWeek, day, month, year } = this.convertDate(
+                order.date
+              );
+              const date = `${dayWeek} ${day} ${month}`;
+              const price = `${order.total_price}€`;
+              return (
+                <li {...{ className: "accordion-list__item", key }}>
+                  <AccordionItem {...{ date, price, items: order.items }} />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+class AccordionItem extends React.Component {
+  state = {
+    opened: false,
+  };
+
+  render() {
+    const {
+      props: { items, date, price },
+      state: { opened },
+    } = this;
+
+    return (
+      <div
+        {...{
+          className: `accordion-item, ${opened && "accordion-item--opened"}`,
+          onClick: () => {
+            this.setState({ opened: !opened });
+          },
+        }}
+      >
+        <div {...{ className: "accordion-item__line" }}>
+          <h3 {...{ className: "accordion-item__title" }}>
+            {date}{" "}
+            <h4 {...{ className: "accordion-item__price" }}>Total: {price}</h4>
+          </h3>
+          <span {...{ className: "accordion-item__icon" }} />
+        </div>
+        <div {...{ className: "accordion-item__inner" }}>
+          <div {...{ className: "accordion-item__content" }}>
+            {items.map((item, i) => {
+              return (
+                <p key={i} {...{ className: "accordion-item__product" }}>
+                  {item._id}
+                </p>
+              );
+            })}
+          </div>
+        </div>
       </div>
     );
   }

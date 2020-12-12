9b3c4ae4c339ec43055ef27949b0d1322acb57d7
kardelen-karatas <kardelen.karatas@gmail.com>
2020-12-12 13:14:58 +0100

    -public -viezs

--- app.js
+++ app.js
@@ -64,16 +64,9 @@ io.on("connection", (socket) => {
 });
 
 
-// Express View engine setup
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
       
 
 app.set('views', path.join(__dirname, 'views'));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local

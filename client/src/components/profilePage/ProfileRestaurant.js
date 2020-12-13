f8b9ede87d9ed6685754841000290bb24645750e
Clementine <clementinenocton@hotmail.fr>
2020-12-13 19:27:18 +0100

    Up

--- client/src/components/profilePage/ProfileRestaurant.js
+++ client/src/components/profilePage/ProfileRestaurant.js
@@ -1,6 +1,6 @@
 import React, { createRef, Component } from "react";
 import { Link } from "react-router-dom";
-import Axios from "axios";
+import axios from "axios";
 import StyledContentLoader from 'styled-content-loader';
 
 import PieChart, {
